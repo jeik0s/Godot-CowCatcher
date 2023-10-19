19530f93f8b70775aa9dcbfd4fc0efa8b17ae3c2
Jakub Zaremba <jzaremba@iterative.pl>
2023-10-19 21:13:59 +0200

    Add background, background collision, cow https://youtu.be/Luf2Kr5s3BM?si=rUGG-s6gPvV2GCFo&t=3426

--- characters/player_cat.gd
+++ characters/player_cat.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
+@onready var state_machine_direction_x = animation_tree.get("parameters/playback/Walk/x")
+@onready var state_machine_direction_y = animation_tree.get("parameters/playback/Walk/y")
 
 func _ready():
 	update_animation_parameters(starting_direction)
@@ -30,7 +32,7 @@ func update_animation_parameters(move_input : Vector2):
 	# dont change animation parameters if there is no move input
 	if(move_input != Vector2.ZERO):
 		animation_tree.set("parameters/Idle/blend_position", move_input)
-		animation_tree.set("parameters/Idle/blend_position", move_input)
+		animation_tree.set("parameters/Walk/blend_position", move_input)
 
 func pick_new_state():
 	if(velocity != Vector2.ZERO):
